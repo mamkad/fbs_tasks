7f9db4985b0dad6fb824c9fe5e71054759785fea
mamkad <89218729+mamkad@users.noreply.github.com>
2021-12-18 15:43:05 +0300

    Add files via upload

---
+++ gRPC/client.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+	"google.golang.org/grpc"
+	"./grpcserver"
+	"context"
+)
+
+func main() {
+	x := 6
+	y := 456
+	
+	con, err := grpc.Dial(":8080",grpc.WithInsecure())
+	
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	c := GRPCapi.NewFibonacciClient(con)
+	res, err := c.Get(context.Background(), &GRPCapi.FibRequest{X:int32(x), Y :int32(y)})
+	
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	log.Println(res.GetResult())
+}
